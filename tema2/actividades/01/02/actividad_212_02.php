480dfb93f81a8a38c49f7ef292c0c0106bbd3294
SpacialRamen <pablobocanegracorrales@gmail.com>
2026-09-24 14:03:06 +0200

    actividades 1,2,3

---
+++ tema2/actividades/01/02/actividad_212_02.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Título</h1>";
+
+        echo "<p> Primera Linea.<br>
+            Linea 2.<br>
+            Linea 3</p>";
+
+        echo '<a href="http://www.elpais.es" target="_blank">Visitar El País</a>';
+
+        echo "<br><br>";
+
+        echo '<img src="https://imagenes.elpais.com/resizer/v2/QFT4YB372FB3VE6Z3FDB7EJEMU.jpg?auth=706cf1d2bea62afc02982bbc8f3f3319c76574937c0fcafeb68f4a1615d2a514&width=1000" 
+        alt="Imagen de El País" width="300">';
+        ?>
+</body>
+</html>
---
+++ tema2/actividades/01/03/actividad_213_02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $texto1 = "Primer texto.";
+        $texto2 = " Segundo texto.";
+
+        $resultado = $texto1 . $texto2;
+
+        echo $resultado;
+    ?>
+</body>
+</html>
